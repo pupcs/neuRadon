1d00548b9ede2c60bf35be426694dbcb8d75417e
pupcs <pupcsongor2002@gmail.com>
2025-08-24 23:40:33 +0200

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+
+
+def config_parser():
+
+    import configargparse
+    return parser
+
+
+def train():
+    parser = config_parser()
+    args = parser.parse_args()
+    
+    if args.random_seed is not None:
+        print('Fixing random seed', args.random_seed)
+        np.random.seed(args.random_seed)
+        tf.compat.v1.set_random_seed(args.random_seed)
+
+    print('e')
+
+
+
+
+if __name__ == '__main__':
+    train()
